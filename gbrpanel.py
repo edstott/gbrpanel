9b35532f4596de9f63f99414fec537fc2b2a2d3d
estott <ed.stott@ic.ac.uk>
2016-02-03 12:19:36 +0000

    Turned pairs finder into function. Paneliser loops through pairs. Added support for macros.

--- gbrpanel.py
+++ gbrpanel.py
@@ -1,6 +1,9 @@
 import re
 import glob
 import os
+import subprocess
+
+import pairs
 
 #filemask = '*.[Gg]*'
 outdir = 'out'
@@ -9,9 +12,12 @@ indir = 'test'
 
 filetypes = ['GBL','GBS','GTL','GTO','GTS','Outline']
 
-AP_RE = re.compile('\%AD(D\d+)([RCO],\d+.\d+(X\d+.\d+)?)\*\%')	#Aperture definition
+AP_RE = re.compile('\%AD(D\d+)([RCOP],\d+\.\d+(X\d+(\.\d+)?)*)\*\%')	#Aperture definition
+APM_RE = re.compile('\%AD(D\d+)([a-zA-Z]\w*)\*\%')	#Aperture macro definition
+AM_RE = re.compile('\%AM([a-zA-Z]\w*)\*') #Begin Aperture Macro
+AMP_RE = re.compile('(1|21|4),(-?\d+(\.\d+)?,)*(-?\d+(\.\d+)?\*)') #Primitive code in macro
 D_RE = re.compile('(D(\d+))\*')	#Operation
-C_RE = re.compile('([XY])(\d+)(Y(\d+))?(I(-?\d+))?(J(-?\d+))?(D0[12])\*') #Draw commands
+C_RE = re.compile('([XY])(-?\d+)(Y(-?\d+))?(I(-?\d+))?(J(-?\d+))?(D0[12])\*') #Draw commands
 G_RE = re.compile('G(\d\d)(.*)\*') #Graphics, modes, comments
 FS_RE = re.compile('\%FSLAX(\d)(\d)Y(\d)(\d)\*\%') #Format specification
 MO_RE = re.compile('\%MO(IN|MM)\*\%') #Units specification
@@ -28,9 +34,11 @@ DEF_PREC = (5,5)
 DEF_FORMAT = ('X{:d}','Y{:d}')
 DEF_OFF_FORMAT = ('I{:d}','J{:d}')
 
+CHECK_DIMS = False
+
 boardpitch = (100,100)
 boarddims = (100,100)
-paneldims = (2,2)
+paneldims = (10,10)
 
 def WriteGerber():	
 	with open(os.path.join(outdir,outfile+str(panelidx)+'.'+ft),'w') as file:
@@ -47,6 +55,13 @@ def WriteGerber():
 		else:
 			file.write('%MOIN*%\n')
 			
+		#Write macros
+		for m,d in outamdict.items():
+			file.write('%AM'+m+'*\n')
+			for p in d['Macro']:
+				file.write(p)
+			file.write('%\n')
+			
 		#Write apertures
 		for a,d in outapdict.items():
 			file.write('%AD'+d+a+'*%\n')
@@ -57,156 +72,205 @@ def WriteGerber():
 			
 		#Write EOFError
 		file.write('M02*\n')
-
-for ft in filetypes:	#Loop over gerber file extensions
-	print('File Extension .'+ft)
-	files = glob.glob(os.path.join(indir,'*.'+ft))
-
-	panelidx = 0
-
-	#Set up the first panel
-	boardidx = 0
-	outapdict = {}
-	outapDidx = 10
-
-	outgerber = []
-	outdigits = DEF_DIGITS
-	outformat = DEF_FORMAT
-	outoffformat = DEF_OFF_FORMAT
-	outprec = DEF_PREC
-	print(' Panel '+str(panelidx))
-
-	for filename in files:	#Loop over files with the correct extension
-
-		inapmap = {}
-		comments = []
-		digits = DEF_DIGITS
-		inprec = DEF_PREC
-		informat = DEF_FORMAT
-		MOMM = True
 		
-		#Set up board origin based on index and dimensions
-		boardorigin = (int(boardidx/paneldims[0])*boardpitch[0],boardidx%paneldims[0]*boardpitch[1])
-		print('  Board '+str(boardidx))
+if __name__ == '__main__':
 
-		with open(filename,'r') as file:
+	pairs = pairs.getPairs()
+	#pairs = pairs[0:5]
+
+	for ft in filetypes:	#Loop over gerber file extensions
+		print('File Extension .'+ft)
+		filename = 'PCB.'+ft
+
+		panelidx = 0
+
+		#Set up the first panel
+		boardidx = 0
+		outapdict = {}
+		outapDidx = 10
+		outamdict = {}
+		inamdict = {}
+
+		outgerber = []
+		outdigits = DEF_DIGITS
+		outformat = DEF_FORMAT
+		outoffformat = DEF_OFF_FORMAT
+		outprec = DEF_PREC
+		#print(' Panel '+str(panelidx))
+
+		for pair in pairs:
+		
+			inapmap = {}
+			comments = []
+			digits = DEF_DIGITS
+			inprec = DEF_PREC
+			informat = DEF_FORMAT
+			MOMM = True
+			currentMacro = None
 			
-			for line in file:
+			#Set up board origin based on index and dimensions
+			boardorigin = (int(boardidx/paneldims[0])*boardpitch[0],boardidx%paneldims[0]*boardpitch[1])
+			#print('  Board '+str(boardidx)+': group '+pair[0]+' zip '+pair[1])
 			
-				lineok = False
-			
-				m = AP_RE.match(line)	#Check for aperture definition
-				if m:
-					if m.group(2) not in outapdict:	#Does this aperture already exist?
-						outapdict[m.group(2)] = 'D'+str(outapDidx)  #If not, add to output dictionary
+			#Extract file
+			subprocess.run(['7z','e',pair[1],'-y','Gerber/'+filename],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+
+			with open(filename,'r') as file:
+				
+				for line in file:
+				
+					lineok = False
+				
+					m = AP_RE.match(line)	#Check for aperture definition
+					if m:
+						if m.group(2) not in outapdict:	#Does this aperture already exist?
+							outapdict[m.group(2)] = 'D'+str(outapDidx)  #If not, add to output dictionary
+							outapDidx += 1
+						inapmap[m.group(1)] = outapdict[m.group(2)] #Add a map from file D-number to output D-number
+						lineok = True
+						
+					m = APM_RE.match(line)	#Check for aperture macro definition
+					if m:
+						macro = inamdict[m.group(2)] #Find new name for this macro
+						outapdict[macro] = outamdict[macro]['MacroD'] #Create an output mapping for the macros D code
+						inapmap[m.group(1)] = outapdict[macro] #Create a D code mapping
+						lineok = True
+						
+					m = AM_RE.match(line) #Macro definition
+					if m:
+						currentMacroD = 'D'+str(outapDidx) #Give the macro a new D code
 						outapDidx += 1
-					inapmap[m.group(1)] = outapdict[m.group(2)] #Add a map from file D-number to output D-number
-					lineok = True
+						currentMacro = 'MACRO'+currentMacroD #Give the macro a new name
+						outamdict[currentMacro] = {'MacroD':currentMacroD, 'Macro':[]} #Initialise a new entry in macro dictionary
+						inamdict[m.group(1)] = currentMacro #Relate the original name to the new name	
+						lineok = True
+						
+					m = AMP_RE.match(line) #Match a macro primitive
+					if m and currentMacro: #Only valid if a macro is open
+						outamdict[currentMacro]['Macro'] += [line]
+						lineok = True
+						
+					if line == '%\n' and currentMacro: #End of a macro
+						currentMacro = None	
+						lineok = True				
+						
+					m = D_RE.match(line) #Check for a D code
+					if m:
+						if int(m.group(2))>9: #D-code applies an aperture
+							outgerber += [inapmap[m.group(1)]+'*'] #Translate the aperture
+							lineok = True
+						elif m.group(2) in OK_Ds:
+							outgerber += [m.group(1)+'*'] #Just copy reserved D-codes (below 10)
+							lineok = True
+							
+					m = C_RE.match(line)	#Check for a coordinate
+					if m:	#'([XY])(\d+)(Y(\d+))?(I(-?\d+))?(J(-?\d+))?(D0[12])\*'
+						outstr = ''
+						xcoord = 0
+						ycoord = 0
+						if m.group(1)=='Y': #Y coordinate only
+							ycoord = float(m.group(2))*scale[1]+boardorigin[1]*10**outprec[1]
+							outstr = outformat[1].format(int(ycoord))
+						elif m.group(3): #X and Y coordinates
+							xcoord = float(m.group(2))*scale[0]+boardorigin[0]*10**outprec[0]
+							ycoord = float(m.group(4))*scale[1]+boardorigin[1]*10**outprec[1]
+							outstr = outformat[0].format(int(xcoord))+outformat[1].format(int(ycoord))
+						else: #X coordinate only
+							xcoord = float(m.group(2))*scale[0]+boardorigin[0]*10**outprec[0]
+							outstr = outformat[0].format(int(xcoord))
+						if m.group(5):	#I offset present
+							icoord = float(m.group(6))*scale[0]
+							outstr += outoffformat[0].format(int(icoord))
+						if m.group(7):	#J offset present
+							jcoord = float(m.group(8))*scale[1]
+							outstr += outoffformat[1].format(int(jcoord))
+						outstr += m.group(9)+'*'
+						
+						if CHECK_DIMS:
+							if xcoord/(10**outprec[0]) < boardorigin[0]:
+								print("Drawing X outside allowed region "+str(xcoord/(10**outprec[0]))+'<'+str(boardorigin[0]))
+							elif xcoord/(10**outprec[0]) > boardorigin[0]+boarddims[0]:
+								print("Drawing X outside allowed region "+str(xcoord/(10**outprec[0]))+'>'+str(boardorigin[0]+boarddims[0]))
+							elif ycoord/(10**outprec[1]) < boardorigin[1]:
+								print("Drawing Y outside allowed region "+str(ycoord/(10**outprec[1]))+'<'+str(boardorigin[1]))
+							elif ycoord/(10**outprec[1]) > boardorigin[1]+boarddims[1]:
+								print("Drawing Y outside allowed region "+str(ycoord/(10**outprec[1]))+'>'+str(boardorigin[1]+boarddims[1]))
+							else:
+								outgerber += [outstr]				
+								lineok = True
+						else:
+							outgerber += [outstr]				
+							lineok = True
 					
-				m = D_RE.match(line) #Check for a D code
-				if m:
-					if int(m.group(2))>9: #D-code applies an aperture
-						outgerber += [inapmap[m.group(1)]+'*'] #Translate the aperture
-						lineok = True
-					elif m.group(2) in OK_Ds:
-						outgerber += [m.group(1)+'*'] #Just copy reserved D-codes (below 10)
-						lineok = True
-						
-				m = C_RE.match(line)	#Check for a coordinate
-				if m:	#'([XY])(\d+)(Y(\d+))?(I(-?\d+))?(J(-?\d+))?(D0[12])\*'
-					outstr = ''
-					xcoord = 0
-					ycoord = 0
-					if m.group(1)=='Y': #Y coordinate only
-						ycoord = int(m.group(2))*scale[1]+boardorigin[1]*10**outprec[1]
-						outstr = outformat[1].format(ycoord)
-					elif m.group(3): #X and Y coordinates
-						xcoord = int(m.group(2))*scale[0]+boardorigin[0]*10**outprec[0]
-						ycoord = int(m.group(4))*scale[1]+boardorigin[1]*10**outprec[1]
-						outstr = outformat[0].format(xcoord)+outformat[1].format(ycoord)
-					else: #X coordinate only
-						xcoord = int(m.group(2))*scale[0]+boardorigin[0]*10**outprec[0]
-						outstr = outformat[0].format(xcoord)
-					if m.group(5):	#I offset present
-						icoord = int(m.group(6))*scale[0]
-						outstr += outoffformat[0].format(icoord)
-					if m.group(7):	#J offset present
-						jcoord = int(m.group(8))*scale[1]
-						outstr += outoffformat[1].format(jcoord)
-					outstr += m.group(9)+'*'
+					m = G_RE.match(line)	#Check for G code
+					if m:
+						if m.group(1) == '04':	#G04 is a comment
+							if m.group(2) not in comments:
+								comments += [m.group(2)]
+							lineok = True
+						elif m.group(1) in STREAM_Gs:	#Add these Gs to output stream
+							outgerber += ['G'+m.group(1)+'*']
+							lineok = True
+						elif m.group(1) in IGNORE_Gs: #Ignore these Gs
+							lineok = True
+							
+					m = FS_RE.match(line)	#Coordinate spec
+					if m:
+							digits = (int(m.group(1))+int(m.group(2)),int(m.group(3))+int(m.group(4)))
+							inprec = (int(m.group(2)),int(m.group(4)))
+							informat = ('X{:0'+str(digits[0])+'d}','Y{:0'+str(digits[1])+'d}')
+							if MOMM:
+								scale = (10**(outprec[0]-inprec[0]),10**(outprec[1]-inprec[1]))
+							else:
+								scale = (25.4*10**(outprec[0]-inprec[0]),25.4*10**(outprec[1]-inprec[1]))
+							lineok = True
+							
+					m = MO_RE.match(line)	#Units spec
+					if m:
+						if m.group(1) == 'MM':
+							MOMM = True
+							scale = (10**(outprec[0]-inprec[0]),10**(outprec[1]-inprec[1]))
+							lineok = True
+						elif m.group(1) == 'IN':
+							MOMM = False
+							scale = (25.4*10**(outprec[0]-inprec[0]),25.4*10**(outprec[1]-inprec[1]))
+							lineok = True
 					
-					if xcoord/(10**outprec[0]) < boardorigin[0]:
-						print("Drawing X outside allowed region "+str(xcoord/(10**outprec[0]))+'<'+str(boardorigin[0]))
-					elif xcoord/(10**outprec[0]) > boardorigin[0]+boarddims[0]:
-						print("Drawing X outside allowed region "+str(xcoord/(10**outprec[0]))+'>'+str(boardorigin[0]+boarddims[0]))
-					elif ycoord/(10**outprec[1]) < boardorigin[1]:
-						print("Drawing Y outside allowed region "+str(ycoord/(10**outprec[1]))+'<'+str(boardorigin[1]))
-					elif ycoord/(10**outprec[1]) > boardorigin[1]+boarddims[1]:
-						print("Drawing Y outside allowed region "+str(ycoord/(10**outprec[1]))+'>'+str(boardorigin[1]+boarddims[1]))
-					else:
-						outgerber += [outstr]				
+					m = LP_RE.match(line)	#New level
+					if m:
+						outgerber += [m.group(0)]
 						lineok = True
-				
-				m = G_RE.match(line)	#Check for G code
-				if m:
-					if m.group(1) == '04':	#G04 is a comment
-						if m.group(2) not in comments:
-							comments += [m.group(2)]
-						lineok = True
-					elif m.group(1) in STREAM_Gs:	#Add these Gs to output stream
-						outgerber += ['G'+m.group(1)+'*']
-						lineok = True
-					elif m.group(1) in IGNORE_Gs: #Ignore these Gs
+					
+					m = M02_RE.match(line) #End of file
+					if m:
 						lineok = True
 						
-				m = FS_RE.match(line)	#Coordinate spec
-				if m:
-						digits = (int(m.group(1))+int(m.group(2)),int(m.group(3))+int(m.group(4)))
-						inprec = (int(m.group(2)),int(m.group(4)))
-						informat = ('X{:0'+str(digits[0])+'d}','Y{:0'+str(digits[1])+'d}')
-						scale = (10**(outprec[0]-inprec[0]),10**(outprec[1]-inprec[1]))
+					if line == '\n':
 						lineok = True
-						
-				m = MO_RE.match(line)	#Units spec
-				if m:
-					if m.group(1) == 'MM':
-						MOMM = True
-					else:
-						MOMM = False
-						print('Bad units!')
-					lineok = True
-				
-				m = LP_RE.match(line)	#New level
-				if m:
-					outgerber += [m.group(0)]
-					lineok = True
-				
-				m = M02_RE.match(line) #End of file
-				if m:
-					lineok = True
-						
-				if not lineok:
-					print('Unrecognised line: '+line)
+							
+					if not lineok:
+						print('Unrecognised line: '+line)
+						print('  Board '+str(boardidx)+': group '+pair[0]+' zip '+pair[1])
 
-		boardidx += 1
-		if boardidx == paneldims[0]*paneldims[1]:	#Write the output if the panel is full
+			boardidx += 1
+			if boardidx == paneldims[0]*paneldims[1]:	#Write the output if the panel is full
+				WriteGerber()
+				
+				#Reset for the next panel
+				panelidx += 1
+				boardidx = 0
+				outapdict = {}
+				outapDidx = 10
+
+				outgerber = []
+				outdigits = DEF_DIGITS
+				outformat = DEF_FORMAT
+				outoffformat = DEF_OFF_FORMAT
+				outprec = DEF_PREC
+				#print(' Panel '+str(panelidx))
+
+		#Write any partially-full final panel
+		if boardidx > 0:
 			WriteGerber()
-			
-			#Reset for the next panel
-			panelidx += 1
-			boardidx = 0
-			outapdict = {}
-			outapDidx = 10
-
-			outgerber = []
-			outdigits = DEF_DIGITS
-			outformat = DEF_FORMAT
-			outoffformat = DEF_OFF_FORMAT
-			outprec = DEF_PREC
-			print(' Panel '+str(panelidx))
-
-	#Write any partially-full final panel
-	if boardidx > 0:
-		WriteGerber()
 		
 	
--- pairs.py
+++ pairs.py
@@ -8,48 +8,51 @@ LOGIN_FILE = 'data/2-login.csv'
 #Extract 'surname,A.B.C.' from 'surname,A.B.C. (given name)' or '***' from '*** NO PARTNER ***'
 NAME_RE = re.compile('(.+)(\s\(.+\)| NO PARTNER)')
 
-#Read all pairs into a list
-with open(PAIR_FILE,'rb') as f:
-	pairs = [row for row in csv.DictReader(f,('Name1','Name2','Group'))]
+def getPairs():
 
-#Read all names into dictionary with logins
-with open(LOGIN_FILE,'rb') as f:
-	logins = {row['Namei']: row['login'] for row in csv.DictReader(f)}
-	
-logins['***'] = ''
-files = {}
+	#Read all pairs into a list
+	with open(PAIR_FILE,'r') as f:
+		pairs = [row for row in csv.DictReader(f,('Name1','Name2','Group'))]
 
-#Loop over pairs
-for pair in pairs:
-	login = []
-	
-	#Find the login for each partner
-	for partner in [pair['Name1'],pair['Name2']]:
-		try:
-			pname = NAME_RE.match(partner).group(1)
-		except AttributeError:
-			print 'Bad Name: '+partner
-			pname = ''
-		try:
-			login += [logins[pname]]
-		except KeyError:
-			print 'login not found for '+pname
-			login += ['']
-	
-	#Look for zip file for each partner, using first in preference
-	pfile =  glob.glob('data/'+login[0]+'.zip')
-	if not pfile:
-		pfile =  glob.glob('data/'+login[1]+'.zip')
-	
-	#Add zip file to output dictionary if it exists
-	if pfile:
-		#print pair[2]+': '+pfile[0]
-		files[pair['Group']] = pfile[0]
-	else:
-		print pair['Group']+': NO FILE'
-		#files[pair[2]] = None
+	#Read all names into dictionary with logins
+	with open(LOGIN_FILE,'r') as f:
+		logins = {row['Namei']: row['login'] for row in csv.DictReader(f)}
+		
+	logins['***'] = ''
+	files = []
 
-print str(len(files))+' Groups with submissions'
+	#Loop over pairs
+	for pair in pairs:
+		login = []
+		
+		#Find the login for each partner
+		for partner in [pair['Name1'],pair['Name2']]:
+			try:
+				pname = NAME_RE.match(partner).group(1)
+			except AttributeError:
+				print('Bad Name: '+partner)
+				pname = ''
+			try:
+				login += [logins[pname]]
+			except KeyError:
+				print('login not found for '+pname)
+				login += ['']
+		
+		#Look for zip file for each partner, using first in preference
+		pfile =  glob.glob('data/'+login[0]+'.zip')
+		if not pfile:
+			pfile =  glob.glob('data/'+login[1]+'.zip')
+		
+		#Add zip file to output dictionary if it exists
+		if pfile:
+			#print pair[2]+': '+pfile[0]
+			files += [(pair['Group'],pfile[0])]
+		else:
+			print(pair['Group']+': NO FILE')
+			#files[pair[2]] = None
+
+	print(str(len(files))+' Groups with submissions')
+	return files
 	
 
 	
